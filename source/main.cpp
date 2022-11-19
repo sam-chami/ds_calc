71f54869aba0457f9559825fee195494bc1c8ae6
Samuel <samasasamu@protonmail.com>
2022-11-19 10:38:24 +0100

    Added multiple digits
    Need to fix repeating numbers

--- source/main.cpp
+++ source/main.cpp
@@ -25,7 +25,7 @@ int main(void)
 	int buttons[23][5] = { {0, 1, 3 ,4, 7},		//
 			       {0, 5, 3, 8, 4},		//  0-9 = number
 			       {0, 10, 3, 12, 1},	//  -1 = dot
-                               {0, 14, 3, 17, 0},	//  31 = pi
+                               {0, 14, 3, 17, 0},	//  -11 = pi
                                {4, 1, 7, 4, 8},		//  -2 = substract
                                {4, 5, 7, 8, 5},		//  -3 = add
                                {4, 10, 7, 12, 2},	//  -4 = multiply
@@ -33,7 +33,7 @@ int main(void)
 			       {8, 1, 10, 4, 9},        //  -6 = power
                                {8, 5, 10, 8, 6},        //  -7 = root
                                {8, 10, 10, 12, 3},      //  -8 = log 
-                               {8, 14, 10, 17, 31},     //  -9 = backspace
+                               {8, 14, 10, 17, -11},     //  -9 = backspace
 			       {12, 1, 15, 4, -5},      //  -10 = equal
                                {12, 5, 15, 8, -4},      //
                                {12, 10, 15, 12, -3},
@@ -49,7 +49,7 @@ int main(void)
 
 	int termA = 0;
 	int termB = 0;
-	bool termComp = false; // Determines if the current Term is complete
+	bool firstDigit = true;
 	bool termCurrent = true; // Determines the current term (true = A, false = B)
 	int operation; // The operation we are performing (+, -, x, ^)
 	int result = 0;
@@ -69,8 +69,6 @@ int main(void)
         iprintf("\x1b[3;3Hxamuel.xyz");
         iprintf("\x1b[18;3HResult:");
 	
-	iprintf("\x1b[18;26H1");
-
 	// draw keypad bitmap
 	int bg2 = bgInitSub(2, BgType_Bmp8, BgSize_B8_256x256, 0,0);
 	dmaCopy(keypadBitmap, bgGetGfxPtr(bg2), 256*192);
@@ -84,16 +82,14 @@ int main(void)
 	   int i;
 	   for (i = 0; i < 23; i++) {
 	      if (inRange(buttons[i][0], buttons[i][2], touch.px/10) && inRange(buttons[i][1], buttons[i][3], touch.py/10)) {
-	         //iprintf("\x1b[18;26H%d ", buttons[i][4]);
 	      	 if (buttons[i][4] < 0) {
-		    termComp = true;
 		    if (inRange(-2, -8, buttons[i][4])) {
 			operation = buttons[i][4];
 			termCurrent = false;
+			firstDigit = true;
 		    } else {
 		       switch(buttons[i][4]) {
 		          case -1: // dot
-			     termComp = false;
 			     break;
 			  case -9: // remove
                              if (termCurrent) {
@@ -105,30 +101,39 @@ int main(void)
 			  case -10: // equal
 			     result = operate(termA, termB, operation);
 			     if (result != NULL) {
-			        iprintf("\x1b[17;26H%d", result);
+			        iprintf("\x1b[18;24H%d", result);
 			     } else {
-                                iprintf("\x1b[17;17HERROR");
+                                iprintf("\x1b[18;15HERROR");
 			     }
                              //termA = result;
 			     //termB = 0;
 			     //operation = 0;
+			     firstDigit = true;
 			     termCurrent = true;
 			     break;
 		       }
 		    }
 		 } else {
-		    //if (!termComp) {
 		       if (termCurrent) {
-		          termA = buttons[i][4];
+		          if (firstDigit){
+			     termA = buttons[i][4];
+			     firstDigit = false;
+			  } else {
+			     termA = termA * 10 + buttons[i][4];
+			  }
 		       } else {
-		          termB = buttons[i][4];
+		          if (firstDigit){
+                             termB = buttons[i][4];
+			     firstDigit = false;
+                          } else {
+                             termB = termB * 10 + buttons[i][4];
+                          }
 		       }
-		    //}
 		 }
 	      }
 	   }
 
-           iprintf("\x1b[8;5H%d, %d, %d\n", termA, termB, operation);
+           iprintf("\x1b[8;5H%d, %d, %d\n", termA, termB, touch.z1);
 	}
 
 	return 0;
